8f0ae8f0221815727973855b0e6cc743f4e144ae
LRBeaver <GitPass16!>
2016-09-30 08:46:18 -0400

    update

---
+++ while_intro.py
@@ -0,0 +1,19 @@
+# number = 1
+# while number < 11:
+#     print(number)
+#     number+=1
+
+# balance = 1000
+# rate = 1.02
+# years = 0
+#
+# while balance < 5000:
+#     balance *= rate
+#     years += 1
+# print ("It takes " + str(years) + " years to reach $5000.")
+
+for i in [1,2,3,4,5,6,7,8,9,10]:
+    print(i)
+
+for names in ["Bill", "Rob", "John", "Steve"]:
+    print(names)
